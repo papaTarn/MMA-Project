1c578a0c15281066d40b2a94ecb23c58481e03c8
panfana <panfana@kkxcel.com>
2024-11-18 17:29:44 +0700

    no message

--- Meow-Front/src/components/ui/cart/cart.tsx
+++ Meow-Front/src/components/ui/cart/cart.tsx
@@ -75,48 +75,26 @@ export default function Cart() {
     }
   }, [cartResult]);
 
-  interface DataType {
-    key: React.Key;
-    name: string;
-    age: number;
-    address: string;
-  }
-
-  const columns: TableColumnsType<DataType> = [
+  const columns: TableColumnsType<ProductItem> = [
     {
-      title: 'Name',
+      title: '',
       dataIndex: 'name',
     },
     {
-      title: 'Age',
-      dataIndex: 'age',
+      title: 'ราคาต่อชิ้น',
+      dataIndex: 'prodPrice',
     },
     {
-      title: 'Address',
+      title: 'จำนวน',
+      dataIndex: 'qty',
+    },
+    {
+      title: 'ราคารวม',
       dataIndex: 'address',
     },
   ];
 
-  const data: DataType[] = [
-    {
-      key: '1',
-      name: 'John Brown',
-      age: 32,
-      address: 'New York No. 1 Lake Park',
-    },
-    {
-      key: '2',
-      name: 'Jim Green',
-      age: 42,
-      address: 'London No. 1 Lake Park',
-    },
-    {
-      key: '3',
-      name: 'Joe Black',
-      age: 32,
-      address: 'Sydney No. 1 Lake Park',
-    },
-  ];
+  const data: ProductItem[] = cart;
 
 
   return (
@@ -127,49 +105,49 @@ export default function Cart() {
         </Flex>
       ) : (
         <Content className="container">
-          {cart.map(data => (
-            <Table<DataType> columns={columns} size="small" />
-            // <Flex gap="middle" align="start" vertical>
-            //   <Card style={{ width: '100%', marginTop: 16 }} bodyStyle={{ padding: 7 }} key={data.id}>
-            //     <Row gutter={[16, 16]} style={{ padding: 0 }}>
-            //       <Col span={3}>
-            //         <img
-            //           alt={data.prodImg}
-            //           src={`${urlImg}${data.prodImg}`}
-            //           style={{ objectFit: 'cover', borderRadius: '5px' }}
-            //           width={100}
-            //           height={100}
-            //         />
-            //       </Col>
+          <Table<ProductItem> columns={columns} dataSource={data} size="small" />
+          {/* {cart.map(data => (            
+            <Flex gap="middle" align="start" vertical>
+              <Card style={{ width: '100%', marginTop: 16 }} bodyStyle={{ padding: 7 }} key={data.id}>
+                <Row gutter={[16, 16]} style={{ padding: 0 }}>
+                  <Col span={3}>
+                    <img
+                      alt={data.prodImg}
+                      src={`${urlImg}${data.prodImg}`}
+                      style={{ objectFit: 'cover', borderRadius: '5px' }}
+                      width={100}
+                      height={100}
+                    />
+                  </Col>
 
-            //       <Col span={20}>
-            //         <Flex gap="large" align="start" vertical>
-            //           <div style={{ display: 'flex' }}>
-            //             <span>{data.recommendFlag ? <FlagRecommend /> : ''}</span>
-            //             <span style={{ fontWeight: 'bold', paddingLeft: 10 }}>{`ID:${data.id} ${data.prodName}`}</span>
-            //           </div>
-            //           <span style={{ marginLeft: '10px', fontWeight: 'bold', color: '#ff4d00' }}>ราคา: ฿{data.prodPrice}</span>
-            //           <InputNumber
-            //             min={1}
-            //             value={quantity}
-            //             formatter={(value) => `${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
-            //             onChange={value => handleQuantityChange(value || 1, data.prodPrice)} // ถ้า value เป็น null จะตั้งค่าเป็น 1
-            //           />
-            //           <span style={{ marginLeft: '10px', fontWeight: 'bold', color: '#ff4d00' }}>ราคารวม: ฿{`${quantity * data.prodPrice}`}</span>
-            //         </Flex>
-            //       </Col>
-            //       <Col span={1}>
-            //         <div
-            //           style={{ display: 'flex', justifyContent: 'center', alignItems: 'flex-start', height: '100%' }}>
-            //           <span onClick={() => deleteProduct(data.id)}>
-            //             <DeleteOutlined style={{ color: '#F44336', fontSize: '1.2rem', cursor: 'pointer' }} />
-            //           </span>
-            //         </div>
-            //       </Col>
-            //     </Row>
-            //   </Card>
-            // </Flex>
-          ))}
+                  <Col span={20}>
+                    <Flex gap="large" align="start" vertical>
+                      <div style={{ display: 'flex' }}>
+                        <span>{data.recommendFlag ? <FlagRecommend /> : ''}</span>
+                        <span style={{ fontWeight: 'bold', paddingLeft: 10 }}>{`ID:${data.id} ${data.prodName}`}</span>
+                      </div>
+                      <span style={{ marginLeft: '10px', fontWeight: 'bold', color: '#ff4d00' }}>ราคา: ฿{data.prodPrice}</span>
+                      <InputNumber
+                        min={1}
+                        value={quantity}
+                        formatter={(value) => `${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+                        onChange={value => handleQuantityChange(value || 1, data.prodPrice)} // ถ้า value เป็น null จะตั้งค่าเป็น 1
+                      />
+                      <span style={{ marginLeft: '10px', fontWeight: 'bold', color: '#ff4d00' }}>ราคารวม: ฿{`${quantity * data.prodPrice}`}</span>
+                    </Flex>
+                  </Col>
+                  <Col span={1}>
+                    <div
+                      style={{ display: 'flex', justifyContent: 'center', alignItems: 'flex-start', height: '100%' }}>
+                      <span onClick={() => deleteProduct(data.id)}>
+                        <DeleteOutlined style={{ color: '#F44336', fontSize: '1.2rem', cursor: 'pointer' }} />
+                      </span>
+                    </div>
+                  </Col>
+                </Row>
+              </Card>
+            </Flex>
+          ))} */}
         </Content>
       )}
     </React.Fragment>
